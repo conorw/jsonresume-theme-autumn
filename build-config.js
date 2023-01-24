f2e0e97022987b28d4a4ac4c853f5c1e6e42b257
Conor Woods <conorw@gmail.com>
2023-01-24 11:55:44 +0000

    update package

--- build-config.js
+++ build-config.js
@@ -10,7 +10,7 @@ module.exports = {
 
   TEMPLATE_FILENAME: 'resume.hbs',
   HTML_FILENAME: 'index.html',
-  CSS_FILENAME: STYLES_DIR + '/resume.css',
+  CSS_FILENAME: '/styles/resume.css',
 
   RESUME_PATH: __dirname + '/resume.json',
   FALLBACK_RESUME_PATH: __dirname + '/resume-sample.json',
